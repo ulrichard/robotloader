ed243adf18faa618b965f5481ef387a92b405dc8
Richard Ulrich <richi@paraeasy.ch>
2011-12-29 20:11:51 +0100

    first commit

---
+++ deb_ppa_upload.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# build a debian sourcepackage and upload it to the launchpad ppa
+export GPGKEY=DA94BB53
+export DEBEMAIL="richi@paraeasy.ch"
+export DEBFULLNAME="Richard Ulrich"
+
+for DISTRIBUTION in precise oneiric natty maverick 
+do
+	VERSIONSTR=1.5-1~${DISTRIBUTION}
+	sed -i  -e "s/maverick/${DISTRIBUTION}/g" -e "s/natty/${DISTRIBUTION}/g" -e "s/oneiric/${DISTRIBUTION}/g" -e "s/precise/${DISTRIBUTION}/g" debian/changelog
+	dpkg-buildpackage -rfakeroot -S
+	dput ppa:richi-paraeasy/ppa ../robotloader_${VERSIONSTR}_source.changes
+done
