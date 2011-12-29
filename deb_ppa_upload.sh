c42c9ed3f2e78c1dcf200080e29f20ae90e3dc17
Richard Ulrich <richi@paraeasy.ch>
2011-12-29 23:22:03 +0100

    wget http://arexx.com/robot_arm/downloads/RobotArmExamples_MINI.zip

--- deb_ppa_upload.sh
+++ deb_ppa_upload.sh
@@ -6,7 +6,7 @@ export DEBFULLNAME="Richard Ulrich"
 
 for DISTRIBUTION in precise oneiric natty maverick 
 do
-	VERSIONSTR=1.5-3~${DISTRIBUTION}
+	VERSIONSTR=1.5-4~${DISTRIBUTION}
 	sed -i  -e "s/maverick/${DISTRIBUTION}/g" -e "s/natty/${DISTRIBUTION}/g" -e "s/oneiric/${DISTRIBUTION}/g" -e "s/precise/${DISTRIBUTION}/g" debian/changelog
 	dpkg-buildpackage -rfakeroot -S
 	dput ppa:richi-paraeasy/ppa ../robotloader_${VERSIONSTR}_source.changes
