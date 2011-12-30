90aea75ea76d8fd73aed0d8690487c074b14c2c0
Richard Ulrich <richi@paraeasy.ch>
2011-12-30 23:20:26 +0100

    * Added an icon

--- deb_ppa_upload.sh
+++ deb_ppa_upload.sh
@@ -7,10 +7,11 @@ export DEBFULLNAME="Richard Ulrich"
 rm *.zip
 wget http://arexx.com/rp6/downloads/RobotLoader_20100712.zip
 wget http://arexx.com/robot_arm/downloads/RobotArmExamples_MINI.zip
+wget http://arexx.com/robot_arm/downloads/RACS_v1.0.zip
 
 for DISTRIBUTION in precise oneiric natty maverick 
 do
-	VERSIONSTR=1.5-6~${DISTRIBUTION}
+	VERSIONSTR=1.5-7~${DISTRIBUTION}
 	sed -i  -e "s/maverick/${DISTRIBUTION}/g" -e "s/natty/${DISTRIBUTION}/g" -e "s/oneiric/${DISTRIBUTION}/g" -e "s/precise/${DISTRIBUTION}/g" debian/changelog
 	dpkg-buildpackage -rfakeroot -S
 	dput ppa:richi-paraeasy/ppa ../robotloader_${VERSIONSTR}_source.changes
