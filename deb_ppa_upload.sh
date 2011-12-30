ff317da7015ca0975ba874f9ea5bfdc1a7dcb030
Richard Ulrich <Richard.Ulrich@cubx-software.com>
2011-12-30 21:15:49 +0100

    using the script that sets the working directory

--- deb_ppa_upload.sh
+++ deb_ppa_upload.sh
@@ -4,9 +4,13 @@ export GPGKEY=DA94BB53
 export DEBEMAIL="richi@paraeasy.ch"
 export DEBFULLNAME="Richard Ulrich"
 
+rm *.zip
+wget http://arexx.com/rp6/downloads/RobotLoader_20100712.zip
+wget http://arexx.com/robot_arm/downloads/RobotArmExamples_MINI.zip
+
 for DISTRIBUTION in precise oneiric natty maverick 
 do
-	VERSIONSTR=1.5-5~${DISTRIBUTION}
+	VERSIONSTR=1.5-6~${DISTRIBUTION}
 	sed -i  -e "s/maverick/${DISTRIBUTION}/g" -e "s/natty/${DISTRIBUTION}/g" -e "s/oneiric/${DISTRIBUTION}/g" -e "s/precise/${DISTRIBUTION}/g" debian/changelog
 	dpkg-buildpackage -rfakeroot -S
 	dput ppa:richi-paraeasy/ppa ../robotloader_${VERSIONSTR}_source.changes
