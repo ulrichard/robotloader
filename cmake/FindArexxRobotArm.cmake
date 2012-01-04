4756dddf6983085cb52efd1a850d79c7a72e88b3
Richard Ulrich <Richard.Ulrich@cubx-software.com>
2012-01-04 23:10:22 +0100

    statring with compiling the racs uc part with cmake

--- cmake/FindArexxRobotArm.cmake
+++ cmake/FindArexxRobotArm.cmake
@@ -2,7 +2,7 @@
 # This module defines
 #  ArexxRobotArm_LIBRARY, the library to link against
 #  ArexxRobotArm_FOUND, if false, do not try to link to FTGL
-#  ArexxRobotArm_INCLUDE_DIR, where to find headers.
+#  ArexxRobotArm_INCLUDE_DIRS, where to find headers.
 #
 #=============================================================================
 # Copyright 2011 Richard Ulrich.
@@ -41,10 +41,14 @@ FIND_LIBRARY(ArexxRobotArm_LIBRARY
   /usr/share/robotloader/examples/RobotArmLib
 )
 
-# handle the QUIETLY and REQUIRED arguments and set FTGL_FOUND to TRUE if 
+IF(ArexxRobotArm_INCLUDE_DIR)
+	SET(ArexxRobotArm_INCLUDE_DIRS ${ArexxRobotArm_INCLUDE_DIR} ${ArexxRobotArm_INCLUDE_DIR}/RobotArmBase ${ArexxRobotArm_INCLUDE_DIR}/RobotArmI2C)
+ENDIF()
+
+# handle the QUIETLY and REQUIRED arguments and set ArexxRobotArm_FOUND to TRUE if 
 # all listed variables are TRUE
 INCLUDE("FindPackageHandleStandardArgs")
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(ArexxRobotArm  DEFAULT_MSG  ArexxRobotArm_LIBRARY  ArexxRobotArm_INCLUDE_DIR)
 
-MARK_AS_ADVANCED(ArexxRobotArm_LIBRARY ArexxRobotArm_INCLUDE_DIR)
+MARK_AS_ADVANCED(ArexxRobotArm_LIBRARY ArexxRobotArm_INCLUDE_DIR ArexxRobotArm_INCLUDE_DIRS)
 
