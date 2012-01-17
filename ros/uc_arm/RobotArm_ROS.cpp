108a48d71c74aee6bc0084076481c7f3457f5682
Richard Ulrich <richi@paraeasy.ch>
2012-01-17 23:44:54 +0100

    starting to port rosserial to the arexx robot arm

---
+++ ros/uc_arm/RobotArm_ROS.cpp
@@ -0,0 +1,44 @@
+// ArexxRobotArm as a ROS node
+// Looking at the rosserial_arduino and adapting it to the arexx robot arm
+// http://www.ros.org/wiki/rosserial_arduino/Tutorials
+// Richard Ulrich <richi@paraeasy.ch>
+// GPL v3
+
+// robot arm lib
+#include "RobotArmBaseLib.h"
+// ros
+#include <ros.h>
+// std lib
+#include <ctype.h>
+/*****************************************************************************/
+int main(void)
+{
+	initRobotBase(); // Always call this first!	
+	Power_Servos();
+	mSleep(200);
+
+	ros::NodeHandle nh;
+	nh.initNode();
+
+	uint8_t servospeed = 3;	// The speed (Fast 0  ......  10 slow) 
+	// Main loop:
+	while(true)
+	{
+		char cc;
+		receiveBytesToBuffer(1, &cc);
+
+//		recbuf[bufpos++] = cc;
+
+
+		nh.spinOnce();
+
+//		s_Move(servonum, servopos, servospeed);
+
+//		bufpos = 0;
+		continue;
+	}
+	// End of main loop!
+	// ---------------------------------------
+
+	return 0;
+}
---
+++ ros/uc_arm/init.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+rm -rf build
+mkdir build
+cd build
+cmake -G "CodeBlocks - Unix Makefiles" -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_TOOLCHAIN_FILE=/usr/share/cmake-2.8/Modules/arexx_robot_arm_crosscompile.cmake ..
+
+make
+
